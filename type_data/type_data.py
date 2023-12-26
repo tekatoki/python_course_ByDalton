57373952ac4291f1ae138f0eea8bdd4c0ac2205b
tekatoki <danoid2007@gmail.com>
2023-12-26 16:00:15 +0100

    Explainsimple type of data

---
+++ type_data/type_data.py
@@ -0,0 +1,15 @@
+'''
+Different type of simple data that python can manage:
+    String > Text value.
+    Int number > Number value.
+    Float number > Number with decimals value.
+    Boolean > Whether True or False value.
+'''
+
+string = "Text value"
+
+int_number = 1
+
+float_number = 1.2
+
+bolean = True
