d11337e1084bfb4abdfbbed185083aa86babb3f5
tekatoki <danoid2007@gmail.com>
2024-01-04 20:21:16 +0100

    Explain comparison operators on python

---
+++ operators/comparison_op.py
@@ -0,0 +1,26 @@
+'''
+In order to compare the value of any variable on python there are several operators
+to set an statement to see whether the condition we have setted is true or false.
+
+The operators available on python are the following:
+    == > {a} is equal to {b}
+    != > {a} is not equal to {b}
+
+    <  > {a} is less than {b} # ONLY type(int OR float)
+    <= > {a} is less or equal to {b} # ONLY type(int OR float)
+
+    >  > {a} is more than {b} # ONLY type(int OR float)
+    >= > {a} is more or equal to {b} # ONLY type(int OR float) 
+'''
+
+equal_to = 3 == 3 # True
+
+not_equal_to = 3 != 3 # False
+
+less_than = 3 < 2 # False
+
+less_or_equal_to = 3 <= 3 # True
+
+greater_than = 8 > 4 # True
+
+greater_or_equal_to = 3 >= 5 # False
