01832708b5e5ed8e3756e52edc078a42f0e7c4c1
tekatoki <danoid2007@gmail.com>
2024-02-09 22:11:32 +0100

    Explain tuples extra curisities and added both unpacking.py file and tuple_function.py to 2.0

---
+++ variables/2.0/tuple_function.py
@@ -0,0 +1,13 @@
+''' 
+There are some ways to create tuples on python.
+It's also important to mention that tuples are a very useful type of data
+when it comes to create inmutable variables that are only-read. This is due to data 
+management.
+'''
+
+# Using function tuple()
+tuple_function = tuple(['list', 'tuple', 'dictionary'])
+
+# Using only comma 
+tuple_comma = 'dot', 'comma', 2.23
+tuple_one = 'this will only have one value', 
