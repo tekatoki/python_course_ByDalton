466aef0ac0d663e464be4e1f00646f7a3d43931d
tekatoki <danoid2007@gmail.com>
2024-02-10 23:03:10 +0100

    Explain while loops

---
+++ loops/while_loop.py
@@ -0,0 +1,11 @@
+'''
+Whiles are loops that repeats the indented code until a condition is True.
+These are very handful, in the following code you can see an example.
+'''
+
+# Is a CLI program that finished whener you press q or Q
+
+user_keylog = None
+
+while 'q' != user_keylog:
+    user_keylog = input('Press any boton [press q to exit]>')
